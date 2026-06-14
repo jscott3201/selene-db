936ad0720efc2f08ad63055f8b4af98c56bd4dd2
Justin Scott <jscott3201@gmail.com>
2026-06-14 04:23:25 -0400

    test(gql): tighten opengql syntax allowlist

--- crates/selene-gql/tests/opengql_cross_validation.rs
+++ crates/selene-gql/tests/opengql_cross_validation.rs
@@ -10,15 +10,9 @@ const SYNTAX_GAP_ALLOWED: &[&str] = &[
     "create_closed_graph_from_nested_graph_type_(double_colon).gql",
     "create_graph.gql",
     "create_schema.gql",
-    "insert_statement.gql",
-    "match_and_insert_example.gql",
-    "match_with_exists_predicate_(match_block_statement_in_braces).gql",
-    "match_with_exists_predicate_(match_block_statement_in_parentheses).gql",
     "match_with_exists_predicate_(nested_match_statement).gql",
     "session_set_graph_to_current_graph.gql",
     "session_set_graph_to_current_property_graph.gql",
-    "session_set_property_as_value.gql",
-    "session_set_time_zone.gql",
 ];
 
 #[test]
